5f1c7cdd8e64615d3afacb969a21f58148ff0143
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-05-05 16:58:51 +0200

    texte defilant

---
+++ header.php
@@ -0,0 +1,49 @@
+<header>
+<div class="mobile">
+   <div class="header"></div>
+  <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
+  <label for="openSidebarMenu" class="sidebarIconToggle">
+    <div class="spinner diagonal part-1"></div>
+    <div class="spinner horizontal"></div>
+    <div class="spinner diagonal part-2"></div>
+  </label>
+<div id="sidebarMenu">
+    <nav>
+    <ul class="sidebarMenuInner" >
+      <li><span>CATEGORIES</span></li>
+      <li><a href="page xbox.html" target="_blank">XBOX ONE|XBOX SERIES S|X</a></li>
+      <li><a href="page playstation.html" target="_blank">PLAYSTATION 4|5</a></li>
+      <li><a href="page switch.html" target="_blank">SWITCH</a></li>
+      <li><a href="page pc.html" target="_blank">PC</a></li>
+      <li><a href="Enfant.html" target="_blank">ENFANTS</a></li>
+      <li><a href="#" target="_blank">ADULTES</a></li>
+      <li><a href="test-connexion2.html">SE CONNECTER</a></li>
+      <li><a href="formulaire d'inscription.html">CREER UN COMPTE</a></li>
+    </ul>
+    </nav>
+  </div>
+</div>
+    <nav class="grand_ecran" >
+        <ul>
+          <li class="deroulant"><a href="#">PLATEFORMES JEUX &ensp;</a>
+            <ul class="sous">
+              <li><a href="page xbox.html">XBOX ONE|XBOX SERIES S|X</a></li>
+              <li><a href="page playstation.html">PLAYSTATION 4|5</a></li>
+              <li><a href="page switch.html">SWITCH</a></li>
+              <li><a href="page pc.html">PC</a></li>
+            </ul>
+          </li>
+          <li class="deroulant"><a href="#">CATEGORIE &ensp;</a>
+            <ul class="sous">
+              <li><a href="Enfant.html">ENFANTS</a></li>
+              <li><a href="#">ADULTES</a></li>
+              <li><a href="#">CSS float</a></li>
+            </ul>
+          </li>
+          <li><a href="#">JEUX ANCIENS</a></li>
+          <li><a href="test-connexion2.html">SE CONNECTER</a></li>
+          <li><a href="formulaire d'inscription.html">CREER UN COMPTE</a></li>
+        </ul>
+      </nav>
+  </div>
+</header>
